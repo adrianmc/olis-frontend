c136fe5a7ab5ad9b1d8f2cbecf96469e4996217c
Adrian Li <li.adrianmc@gmail.com>
2016-02-19 19:46:15 +0800

    medium editor done

--- app/components/ChatMessageItem.jsx
+++ app/components/ChatMessageItem.jsx
@@ -2,7 +2,31 @@ import React from 'react';
 
 import Avatar from 'material-ui/lib/avatar';
 
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import IconButton from 'material-ui/lib/icon-button';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+
 export default class ChatMessageItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHovering: false,
+    };
+  }
+
+  handleMouseEnter = () => {
+    this.setState({ isHovering: true });
+  }
+
+  handleMouseLeave = () => {
+    this.setState({ isHovering: false });
+  }
+
+  handleItemTouchTap = (event, child) => {
+    console.log('You have selected: ' + child.props.primaryText);
+  }
 
   render() {
 
@@ -15,9 +39,48 @@ export default class ChatMessageItem extends React.Component {
     } = this.props;
     
     const authorClass = selfAuthor ? ' you' : '';
+    const buttonStyle = () => {
+      if (this.state.isHovering) {
+        return ({
+          padding: '0',
+          width: '36px',
+          height: '36px'
+        });
+      } else {
+        return ({
+          padding: '0',
+          width: '36px',
+          height: '36px',
+          opacity: '0',
+          pointerEvents: 'none',
+        });
+      }
+    }
+    const contextMenu = (
+      <IconMenu
+        onItemTouchTap={this.handleItemTouchTap}
+        iconButtonElement={
+          <IconButton 
+            style={buttonStyle()}
+          >
+            <MoreVertIcon color="rgba(0,0,0,0.5)" />
+          </IconButton>
+        }
+        anchorOrigin={{horizontal: 'middle', vertical: 'bottom'}}
+        targetOrigin={{horizontal: 'middle', vertical: 'top'}}
+      >
+        <MenuItem primaryText="Translate" />
+        <MenuItem primaryText="Copy" />
+        <MenuItem primaryText="Lorem Ipsum" />
+      </IconMenu>
+    );
 
     return (
-      <div className={'chat-msg-item' + authorClass}>
+      <div 
+        className={'chat-msg-item' + authorClass}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+      >
         <div className="chat-author">{authorName}</div>
         <div className="chat-primary">
           <div className="chat-avatar">
@@ -28,6 +91,9 @@ export default class ChatMessageItem extends React.Component {
               <p>{content}</p>
             </div>
             <div className="chat-timestamp">{timestamp}</div>
+          </div>
+          <div>
+            {contextMenu}
           </div>
         </div>
       </div>
--- app/components/NotesContainer.jsx
+++ app/components/NotesContainer.jsx
@@ -1,18 +1,59 @@
 import React from 'react';
-import ContentUndo from 'material-ui/lib/svg-icons/content/undo';
-import ContentRedo from 'material-ui/lib/svg-icons/content/redo';
+
+import UndoIcon from 'material-ui/lib/svg-icons/content/undo';
+import RedoIcon from 'material-ui/lib/svg-icons/content/redo';
+import SaveIcon from 'material-ui/lib/svg-icons/content/save';
+import ShareIcon from 'material-ui/lib/svg-icons/social/share';
+
+import IconButton from 'material-ui/lib/icon-button';
+import MediumEditor from 'react-medium-editor/lib/editor';
 
 export default class NotesContainer extends React.Component {
+  handleChange = (text, medium) => {
+    console.log('Content Has Changed')
+    console.log(text)
+  }
+
   render() {
+    const iconColor = 'rgba(0,0,0,0.8)';
+
     return (
       <div id="notes-container">
-        <div className="notes-data-wrapper">
-          <h1>Meeting Notes</h1>
-          <p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>
+        <div className="notes-header">
+          <div className="notes-icon-bar">
+            <IconButton tooltip="Share">
+              <ShareIcon color={iconColor}/>
+            </IconButton>
+            <IconButton tooltip="Save to file...">
+              <SaveIcon color={iconColor}/>
+            </IconButton>
+            <IconButton tooltip="Undo">
+              <UndoIcon color={iconColor}/>
+            </IconButton>
+            <IconButton tooltip="Redo">
+              <RedoIcon color={iconColor}/>
+            </IconButton>
+          </div>
         </div>
-        <div className="notes-icon-bar">
-          <div className="notes-icon notes-undo"><i className="fa fa-fw fa-undo"></i></div>
-          <div className="notes-icon notes-redo"><i className="fa fa-fw fa-undo fa-flip-horizontal"></i></div>
+        <div className="notes-data-wrapper">
+          <MediumEditor
+            text="<h1>Meeting Notes</h1><p>Never in all their history have men been able truly to conceive of the world as one: a single sphere, a globe, having the qualities of a globe, a round earth in which all the directions eventually meet, in which there is no center because every point, or none, is center — an equal earth which all men occupy as equals. The airman's earth, if free men make it, will be truly round: a globe in practice, not in theory.</p>"
+            onChange={this.handleChange}
+            options={{toolbar:
+              {buttons: [
+                'bold',
+                'italic',
+                'underline',
+                'anchor',
+                'h1',
+                'h2',
+                'h3',
+                'quote',
+                'orderedlist',
+                'unorderedlist',
+              ]}
+            }}
+          />
         </div>
       </div>
     );
