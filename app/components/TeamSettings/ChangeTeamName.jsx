7dcebe3c800f1f31fd6fd2cf282933ebd95a608a
Adrian Li <li.adrianmc@gmail.com>
2016-03-01 13:20:48 +0800

    more func

--- app/components/TeamSettings/ChangeTeamName.jsx
+++ app/components/TeamSettings/ChangeTeamName.jsx
@@ -5,6 +5,11 @@ import TextField from 'material-ui/lib/text-field';
 
 export default class ChangeTeamName extends React.Component {
   
+  submitHandler = () => {
+    const input = this._textField.getValue();
+    console.log(input + "is the new name to be set");
+  }
+
   render() {
     const teamName = "The A Team";
     return (
@@ -12,6 +17,7 @@ export default class ChangeTeamName extends React.Component {
         title="Change Team Name"
         subtitle="This is the name of your team."
         submitButtonText="Change Team Name"
+        handleSubmit={this.submitHandler}
         disableReset={true}
         initiallyExpanded={true}
       >
@@ -19,6 +25,7 @@ export default class ChangeTeamName extends React.Component {
         <TextField
           hintText="Choose a new team name"
           floatingLabelText="New Team Name"
+          ref={(x) => this._textField = x}
         />
       </SettingsCard>
     );
--- app/components/TeamSettings/InviteTeammates.jsx
+++ app/components/TeamSettings/InviteTeammates.jsx
@@ -8,42 +8,60 @@ import AddIcon from 'material-ui/lib/svg-icons/content/add-box';
 
 
 export default class InviteTeammates extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      submitted: false,
+    };
+  }
+
+  submitHandler = () => {
+    this.setState({submitted: true});
+  }
   
   render() {
-    return (
-      <div>
-        <p>Enter in the email addresses of the teammates you would like to invite:</p>
+    if (this.state.submitted) {
+      return (
+        <div>Your invites have been sent!</div>
+      );
+    } else {
+      return (
         <div>
-          <TextField
-            hintText="your.name@example.com"
-            floatingLabelText="Email"
-          />
-        </div>
+          <p>Enter in the email addresses of the teammates you would like to invite:</p>
+          <div>
+            <TextField
+              hintText="your.name@example.com"
+              floatingLabelText="Email"
+            />
+          </div>
 
-        <div>
-          <TextField
-            hintText="your.name@example.com"
-            floatingLabelText="Email"
-          />
-        </div>
+          <div>
+            <TextField
+              hintText="your.name@example.com"
+              floatingLabelText="Email"
+            />
+          </div>
 
-        <div>
-          <TextField
-            hintText="your.name@example.com"
-            floatingLabelText="Email"
-          />
-          <IconButton tooltip="Invite More">
-            <AddIcon color="#aaaaaa"/>
-          </IconButton>
-        </div>
+          <div>
+            <TextField
+              hintText="your.name@example.com"
+              floatingLabelText="Email"
+            />
+            <IconButton tooltip="Invite More">
+              <AddIcon color="#aaaaaa"/>
+            </IconButton>
+          </div>
 
-        <div style={{margin:'14px 0'}}>
-          <RaisedButton
-            label="Invite Teammates"
-            secondary={true}
-          />
+          <div style={{margin:'14px 0'}}>
+            <RaisedButton
+              label="Invite Teammates"
+              secondary={true}
+              onClick={this.submitHandler}
+            />
+          </div>
         </div>
-        </div>
-    );
+      );
+    }
   }
 }
